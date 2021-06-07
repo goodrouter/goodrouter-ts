0156e251a944801e83e42d93abbd2cdf905d86bd
Elmer Bulthuis <elmerbulthuis@gmail.com>
2021-06-07 16:10:45 +0200

    testing

--- src/router.spec.ts
+++ src/router.spec.ts
@@ -4,9 +4,28 @@ import { Router } from "./router.js";
 
 test("router", async t => {
     const router = new Router();
-    router.insertRoute("one", "/one");
-    const route = router.findRoute("/one");
-    assert(route);
-    t.equal(route.name, "one");
+    router.insertRoute("one", "/a");
+    router.insertRoute("two", "/a/{x}/{y}");
+    router.insertRoute("three", "/c/{x}");
+
+    {
+        const route = router.findRoute("/a");
+        assert(route);
+        t.equal(route.name, "one");
+    }
+
+    {
+        const route = router.findRoute("/a/1/2");
+        assert(route);
+        t.equal(route.name, "two");
+        t.deepEqual(route.parameters, { x: "1", y: "2" });
+    }
+
+    {
+        const route = router.findRoute("/c/3");
+        assert(route);
+        t.equal(route.name, "three");
+        t.deepEqual(route.parameters, { x: "3" });
+    }
 });
 
