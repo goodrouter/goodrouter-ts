5d9e91884fbcf323b19b5d4987bb0366679a3dc1
Elmer Bulthuis <elmerbulthuis@gmail.com>
2022-02-03 22:16:57 +0100

    cleanup

--- src/route-node.ts
+++ src/route-node.ts
@@ -99,226 +99,6 @@ export function parseRoute(
     return null;
 }
 
-export function getRootNode(node: RouteNode) {
-    while (node.parent) {
-        node = node.parent;
-    }
-    return node;
-}
-
-export function makeRouteNode(
-    name: string,
-    template: string,
-) {
-    const parts = Array.from(emitTemplatePathParts(template));
-
-    const anchor = parts.pop()!;
-    const parameter = parts.pop() ?? null;
-
-    const node: RouteNode = {
-        name,
-        anchor,
-        parameter,
-        children: [],
-        parent: null,
-    };
-    let rootNode = node;
-
-    while (parts.length > 0) {
-        const anchor = parts.pop()!;
-        const parameter = parts.pop() ?? null;
-
-        const parentNode = {
-            anchor,
-            name: null,
-            parameter,
-            children: [rootNode],
-            parent: null,
-        };
-        rootNode.parent = parentNode;
-        rootNode = parentNode;
-    }
-
-    return node;
-}
-
-export function routeNodeOrder(a: RouteNode, b: RouteNode) {
-    if ((a.parameter == null) > (b.parameter == null)) return -1;
-    if ((a.parameter == null) < (b.parameter == null)) return 1;
-
-    if ((a.name == null) > (b.name == null)) return -1;
-    if ((a.name == null) < (b.name == null)) return 1;
-
-    if (a.anchor.length > b.anchor.length) return -1;
-    if (a.anchor.length < b.anchor.length) return 1;
-
-    if (a.anchor > b.anchor) return -1;
-    if (a.anchor < b.anchor) return 1;
-
-    return 0;
-}
-
-export function routeNodeEqual(a: RouteNode, b: RouteNode) {
-    return (
-        a.name === b.name &&
-        a.anchor === b.anchor &&
-        a.parameter === b.parameter
-    );
-}
-
-export function optimizeRouteNode(newNode: RouteNode) {
-    const parentNode = newNode.parent;
-    if (!parentNode) throw new Error("cannot optimize root node");
-
-    const newNodeIndex = parentNode.children.indexOf(newNode);
-
-    // First, find a similar node, a route with a common anchor prefix
-
-    let similarNodeIndex = -1;
-    let similarNode: RouteNode | null = null;
-    let commonPrefix = "";
-
-    for (
-        let childNodeIndex = 0;
-        childNodeIndex < parentNode.children.length;
-        childNodeIndex++
-    ) {
-        if (childNodeIndex === newNodeIndex) continue;
-
-        const childNode = parentNode.children[childNodeIndex];
-
-        const commonPrefixLength = findCommonPrefixLength(newNode.anchor, childNode.anchor);
-        if (commonPrefixLength === 0) continue;
-
-        commonPrefix = newNode.anchor.substring(0, commonPrefixLength);
-        similarNodeIndex = childNodeIndex;
-        similarNode = childNode;
-
-        break;
-    }
-
-    if (similarNode == null) {
-        // no similar node found! cannot optimize this node
-        return;
-    }
-
-    // now that we found a similar node, lets figure out what strategy we need to merge
-
-    if (newNode.anchor === similarNode.anchor) {
-        if (
-            newNode.name != null &&
-            similarNode.name != null &&
-            newNode.name !== similarNode.name
-        ) {
-            throw new Error("ambiguous route");
-        }
-
-        if (
-            newNode.parameter != null &&
-            similarNode.parameter != null &&
-            newNode.parameter !== similarNode.parameter
-        ) {
-            // this is kind of an edge case, the parameter names are different, but for the rest
-            // the node is the same. We place an intermediate node that groups the two with an
-            // empty anchor.
-
-            const intermediateNode: RouteNode = {
-                name: null,
-                anchor: commonPrefix,
-                parameter: null,
-                children: [
-                    similarNode,
-                    newNode,
-                ],
-                parent: parentNode,
-            };
-            parentNode.children.splice(similarNodeIndex, 1, intermediateNode);
-            parentNode.children.splice(newNodeIndex, 1);
-
-            similarNode.anchor = "";
-            similarNode.parent = intermediateNode;
-
-            newNode.anchor = "";
-            newNode.parent = intermediateNode;
-
-            intermediateNode.children.sort(routeNodeOrder);
-        }
-        else {
-            // The two nodes can be merged! This is great! we merge the names, parameter
-            // and the children
-
-            parentNode.children.splice(newNodeIndex, 1);
-
-            similarNode.name ??= newNode.name;
-            similarNode.parameter ??= newNode.parameter;
-            similarNode.children.push(...newNode.children);
-
-            newNode.children.forEach(child => child.parent = similarNode);
-            newNode.children.forEach(optimizeRouteNode);
-
-            similarNode.children.sort(routeNodeOrder);
-        }
-    }
-    else if (newNode.anchor === commonPrefix) {
-        // in this case the similar node should be child of the new node
-        // because the new node anchor is a prefix of the similar node anchor
-
-        parentNode.children.splice(similarNodeIndex, 1);
-
-        similarNode.anchor = similarNode.anchor.substring(commonPrefix.length);
-
-        newNode.children.push(similarNode);
-        similarNode.parent = newNode;
-
-        optimizeRouteNode(similarNode);
-
-        newNode.children.sort(routeNodeOrder);
-    }
-    else if (similarNode.anchor === commonPrefix) {
-        // this is the exact inverse of the previous clause
-
-        parentNode.children.splice(newNodeIndex, 1);
-
-        newNode.anchor = newNode.anchor.substring(commonPrefix.length);
-
-        similarNode.children.push(newNode);
-        newNode.parent = similarNode;
-
-        optimizeRouteNode(newNode);
-
-        similarNode.children.sort(routeNodeOrder);
-    }
-    else {
-        // we encounteres two nodes that are not the same, and none of the two nodes
-        // has an anchor that is a prefix of the other. Both nodes share a common prefix
-        // in the anchor. We need an intermediate that has the common prefix as the
-        // anchor with the two nodes as children. The common prefix is removed from the
-        // anchor of the two nodes.
-
-        const intermediateNode: RouteNode = {
-            name: null,
-            anchor: commonPrefix,
-            parameter: null,
-            children: [
-                similarNode,
-                newNode,
-            ],
-            parent: parentNode,
-        };
-        parentNode.children.splice(similarNodeIndex, 1, intermediateNode);
-        parentNode.children.splice(newNodeIndex, 1);
-
-        similarNode.anchor = similarNode.anchor.substring(commonPrefix.length);
-        similarNode.parent = intermediateNode;
-
-        newNode.anchor = newNode.anchor.substring(commonPrefix.length);
-        newNode.parent = intermediateNode;
-
-        intermediateNode.children.sort(routeNodeOrder);
-    }
-
-}
-
 export function insertRouteNode(targetNode: RouteNode, name: string, template: string) {
     const chainNodes = [...newRouteNodeChain(name, template)];
     chainNodes.reverse();
@@ -416,7 +196,31 @@ export function insertRouteNode(targetNode: RouteNode, name: string, template: s
     return currentNode;
 }
 
-export function* newRouteNodeChain(name: string, template: string): Iterable<RouteNode> {
+function routeNodeOrder(a: RouteNode, b: RouteNode) {
+    if ((a.parameter == null) > (b.parameter == null)) return -1;
+    if ((a.parameter == null) < (b.parameter == null)) return 1;
+
+    if ((a.name == null) > (b.name == null)) return -1;
+    if ((a.name == null) < (b.name == null)) return 1;
+
+    if (a.anchor.length > b.anchor.length) return -1;
+    if (a.anchor.length < b.anchor.length) return 1;
+
+    if (a.anchor > b.anchor) return -1;
+    if (a.anchor < b.anchor) return 1;
+
+    return 0;
+}
+
+function routeNodeEqual(a: RouteNode, b: RouteNode) {
+    return (
+        a.name === b.name &&
+        a.anchor === b.anchor &&
+        a.parameter === b.parameter
+    );
+}
+
+function* newRouteNodeChain(name: string, template: string): Iterable<RouteNode> {
     const parts = [...emitTemplatePathParts(template)];
     let currentName: string | null = name;
 
