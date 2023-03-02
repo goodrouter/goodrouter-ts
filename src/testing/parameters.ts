dcdbf67db52a15c8a73ee35f78e69a27d6f35967
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-03-02 10:49:56 +0100

    more sanity

--- src/testing/parameters.ts
+++ src/testing/parameters.ts
@@ -6,17 +6,16 @@ export function parametersFromTemplates(
 ): Iterable<string> {
     const parameters = new Set<string>();
     for (const template of templates) {
-        let index = -1;
+        let index = 0;
         for (const part of parseTemplateParts(
             template,
             defaultRouterOptions.parameterPlaceholderRE,
         )) {
-            index++;
-
             if (index % 2 !== 0) {
                 parameters.add(part);
             }
 
+            index++;
         }
     }
 
